75d49f96c576e08656148bb30dc4fa5ab10a5ce6
liyy0 <yuyanli@bu.edu>
2024-02-26 14:17:38 -0500

    finish switch button

--- app/src/main/java/com/example/yuyanli_assignment4q2/MainActivity.kt
+++ app/src/main/java/com/example/yuyanli_assignment4q2/MainActivity.kt
@@ -24,6 +24,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var button_bitcoin = findViewById<TextView>(R.id.button_bitcoin)
+        var button_apple = findViewById<TextView>(R.id.button_apples)
+        button_bitcoin.setOnClickListener {
+            getComments()
+        }
+        button_apple.setOnClickListener {
+            getApple_news()
+        }
+
+
         getComments()
 
     }
@@ -33,7 +43,7 @@ class MainActivity : AppCompatActivity() {
         val recyclerView: RecyclerView = findViewById(R.id.news_recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = ListAdapter(articlesList) {
-            Toast.makeText(this, "Clicked on ${it.title}", Toast.LENGTH_SHORT).show()
+//            Toast.makeText(this, "Clicked on ${it.title}", Toast.LENGTH_SHORT).show()
             val intent = Intent(this, detail_page::class.java)
             intent.putExtra("title", it.title)
             intent.putExtra("content", it.content)
@@ -67,10 +77,41 @@ class MainActivity : AppCompatActivity() {
                     Log.d(TAG, "onResponse: ${news.articles[0].title}")
                 }
             }
-
-
             override fun onFailure(call: Call<News>, t: Throwable) {
                 Log.e(TAG, "onFailure: ${t.message}")
+                Toast.makeText(this@MainActivity, "Error: ${t.message}", Toast.LENGTH_SHORT).show()
+            }
+
+        })
+    }
+
+    private fun getApple_news(){
+//        val textview = findViewById<TextView>(R.id.textView2)
+        val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+        val api = Retrofit.Builder()
+            .baseUrl(base_url)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(MyApi::class.java)
+        api.getApple().enqueue(object :Callback<News>{
+            override fun onResponse(
+                call: Call<News>,
+                response: Response<News>
+            ) {
+                Log.d(TAG, "onResponse: ${response}")
+                if (response.isSuccessful){
+                    val news = response.body()!!
+//                    textview.text = news.articles[0].title
+//                    callback(news)
+                    setupRecyclerView(news.articles)
+                    Log.d(TAG, "onResponse: ${news.articles[0].title}")
+                }
+            }
+            override fun onFailure(call: Call<News>, t: Throwable) {
+                Log.e(TAG, "onFailure: ${t.message}")
+                Toast.makeText(this@MainActivity, "Error: ${t.message}", Toast.LENGTH_SHORT).show()
             }
 
         })
--- app/src/main/java/com/example/yuyanli_assignment4q2/MyApi.kt
+++ app/src/main/java/com/example/yuyanli_assignment4q2/MyApi.kt
@@ -6,4 +6,7 @@ import retrofit2.http.GET
 interface MyApi {
     @GET("everything?q=bitcoin&apiKey=c0814ae1400c40789ceb8852fc1cc59d")
     fun getNews(): Call<News>
+
+    @GET("everything?q=apple&from=2024-02-25&to=2024-02-25&sortBy=popularity&apiKey=c0814ae1400c40789ceb8852fc1cc59d")
+    fun getApple(): Call<News>
 }
